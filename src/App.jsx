430475d63ba04d6498399feb4f7e2473bc187a85
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-03 10:29:18 +0200

    App actualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import './App.css'
 import Header from './components/header/Header';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Portfolio from './components/Portfolio/Work';
+import Home from './components/home/Home';
+import Qualification from './components/qualification/Qualification';
+import ScrollUp from './components/scrollup/ScrollUp';
+import Services from './components/services/Services';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -7,8 +16,17 @@ function App() {
       <Header/>
 
       <main className="main">
-        
+      <Home/>
+      <About/>
+      <Skills/>
+      <Services/>
+      <Qualification/>
+      <Work/>
+      <Contact/>
       </main>
+
+      <Footer/>
+      <ScrollUp/>
     </>
     
   )
