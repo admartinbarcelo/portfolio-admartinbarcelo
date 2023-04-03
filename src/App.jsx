8696dd444f8580ec9564ae96459dbca9844644e4
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-03 12:29:43 +0200

    about me & CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Header from './components/header/Header';
 import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-import Portfolio from './components/Portfolio/Work';
-import Home from './components/home/Home';
-import Qualification from './components/qualification/Qualification';
-import ScrollUp from './components/scrollup/ScrollUp';
-import Services from './components/services/Services';
-import Skills from './components/skills/Skills';
+// import Contact from './components/contact/Contact';
+// import Footer from './components/footer/Footer';
+// import Work from './components/Portfolio/Work';
+// import Home from './components/home/Home';
+// import Qualification from './components/qualification/Qualification';
+// import ScrollUp from './components/scrollup/ScrollUp';
+// import Services from './components/services/Services';
+// import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -16,20 +16,11 @@ function App() {
       <Header/>
 
       <main className="main">
-      <Home/>
       <About/>
-      <Skills/>
-      <Services/>
-      <Qualification/>
-      <Work/>
-      <Contact/>
       </main>
-
-      <Footer/>
-      <ScrollUp/>
     </>
     
   )
 }
 
-export default App
+export default App;
---
+++ src/components/about/Info.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Info () {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">6 Months, including student period, OPEN TO WORK!</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">IronHack Bootcamp<br/>Full-Stack Dev<br/>(MERN Stack)</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-user-check about__icon"></i>
+
+        <h3 className="about__title">My Self</h3>
+        <span className="about__subtitle">inquiring
+            mentality Team work<br/>self-learning</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
