62c79754cac9dd5542cbae7e45031a2843b0cae3
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-08 22:31:44 +0200

    links project done

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -9,18 +9,21 @@ export const projectsData = [
     image: Work1,
     title: "Hands Up",
     category: "Full Stack",
+    demoLink: "https://handsup-project.netlify.app/"
   },
   {
     id: 2,
     image: Work2,
     title: "Clippy",
     category: "Backend",
+    demoLink: "https://clippy1.fly.dev/"
   },
   {
     id: 3,
     image: Work3,
     title: "DB Jump",
     category: "Frontend",
+    demoLink: "https://eogimenez.github.io/dragonBall-Jump/"
   },
 ];
 
--- src/components/Portfolio/WorkItems.jsx
+++ src/components/Portfolio/WorkItems.jsx
@@ -3,7 +3,7 @@ function WorkItems({ item }) {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="images" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
+      <a href={item.demoLink} className="work__button" target="_blank">
         Demo
         <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
