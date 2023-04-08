44181d32416a37cb41f660659e3affc01f9714ce
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-08 21:44:43 +0200

    Projects and photos

---
+++ src/components/Portfolio/Data.jsx
@@ -0,0 +1,41 @@
+import Work3 from "../../assets/DBjump.png";
+import Work2 from "../../assets/Clippy.png";
+import Work1 from "../../assets/Handsup.png";
+
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Hands Up",
+    category: "Full Stack",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Clippy",
+    category: "Backend",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "DB Jump",
+    category: "Frontend",
+  },
+];
+
+// projects
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "Full Stack"
+  },
+  {
+    name: "Frontend",
+  },
+  {
+    name: "Backend",
+  },
+];
