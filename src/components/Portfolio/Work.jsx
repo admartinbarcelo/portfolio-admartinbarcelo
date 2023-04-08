a04a481439f4860a1963e075f16752705c22cc01
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-08 22:00:01 +0200

    Scroll component styles

--- src/components/Portfolio/Work.jsx
+++ src/components/Portfolio/Work.jsx
@@ -5,7 +5,7 @@ function Work () {
     return(
     <section className="work section" id='portfolio'>
         <h2 className="section__title">Portfolio</h2>
-        <span className="section__subtitle">Most recent work</span>
+        <span className="section__subtitle">Most recent works</span>
 
         <Works/>
         
