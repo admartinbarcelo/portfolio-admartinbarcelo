c2c6032d336dd7b3ab707b37f5e157dc6d19665e
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-08 22:18:57 +0200

    scrollup done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './components/about/About';
 import Work from './components/Portfolio/Work';
 import Home from './components/home/Home';
 // import Qualification from './components/qualification/Qualification';
-// import ScrollUp from './components/scrollup/ScrollUp';
+import ScrollUp from './components/scrollup/ScrollUp';
 // import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 
@@ -21,6 +21,8 @@ function App() {
       <Work/>
       <Skills/>
       </main>
+
+    <ScrollUp/>
     </>
     
   )
