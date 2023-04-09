88f7cd205e4cce06c66888fa36d18cbd1de75d4a
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-09 20:17:53 +0200

    services 1.0

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import './services.css';
+import { useState } from "react";
+import "./services.css";
 
-function Services () {
-    const [toggleState, setToggleState] = useState(0);
-    const toggleTab = (index) => {
-        setToggleState(index);
-    }
-    return (
-        <section className="services section" id="services">
+function Services() {
+  const [toggleState, setToggleState] = useState(0);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+    <section className="services section" id="services">
       <h2 className="section__title">Services</h2>
       <span className="section__subtitle">What i offer</span>
 
@@ -15,9 +15,7 @@ function Services () {
         <div className="services__content">
           <div>
             <i className="uil uil-monitor services__icon"></i>
-            <h3 className="services__title">
-              Frontend
-            </h3>
+            <h3 className="services__title">Frontend</h3>
           </div>
 
           <span className="services__button" onClick={() => toggleTab(1)}>
@@ -40,36 +38,43 @@ function Services () {
 
               <h3 className="services__modal-title">Frontend Developer</h3>
               <p className="services__modal-description">
-              I have experience in front-end technologies such as JavaScript, React, HTML, CSS, and Bootstrap, and I have worked on creating and implementing the visual elements of a website. <br />
-              Although I am still gaining experience, I have a solid understanding of the software development lifecycle and best development practices, and I work well under the guidance of more experienced front-end developers. I am dedicated to learning and improving my skill set by staying up-to-date with the latest trends and technologies in the industry.
+                I have experience in front-end technologies such as JavaScript,
+                React, HTML, CSS, and Bootstrap, and I have worked on creating
+                and implementing the visual elements of a website. <br />
+                Although I am still gaining experience, I have a solid
+                understanding of the software development lifecycle and best
+                development practices, and I work well under the guidance of
+                more experienced front-end developers. I am dedicated to
+                learning and improving my skill set by staying up-to-date with
+                the latest trends and technologies in the industry.
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                  Designing and developing attractive user interfaces.
+                    Designing and developing attractive user interfaces.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                  Maintaining and updating existing web pages.
+                    Maintaining and updating existing web pages.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                  Creating responsive web pages.
+                    Creating responsive web pages.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                  Integrating APIs and third-party services.
+                    Integrating APIs and third-party services.
                   </p>
                 </li>
               </ul>
@@ -80,9 +85,7 @@ function Services () {
         <div className="services__content">
           <div>
             <i className="uil uil-server services__icon"></i>
-            <h3 className="services__title">
-              Backend
-            </h3>
+            <h3 className="services__title">Backend</h3>
           </div>
 
           <span onClick={() => toggleTab(2)} className="services__button">
@@ -105,41 +108,43 @@ function Services () {
 
               <h3 className="services__modal-title">Backend Developer</h3>
               <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
+                As a backend developer, I have experience building
+                server-side applications using technologies such as Javascript,
+                NodeJS, ExpressJS, MongoDB, and Postman. I am skilled in
+                creating web servers using NodeJS, and utilizing ExpressJS to
+                handle HTTP requests and routing. In addition, I am proficient
+                in working with MongoDB as a NoSQL database to store and
+                retrieve data. Furthermore, I have experience testing API
+                endpoints with Postman, as well as using npm and Git for package
+                and code management.
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop the user interface.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I create ux element interactions.
+                  Developing server-side applications.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I position your company brand.
+                  Creating and maintaining web servers with NodeJS and ExpressJS
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    Design and mockups of products for companies.
+                  Setting up and configuring databases.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                  Using npm and Git for package and code management
                   </p>
                 </li>
               </ul>
@@ -218,8 +223,7 @@ function Services () {
         </div>
       </div>
     </section>
-
-    )
+  );
 }
 
 export default Services;
