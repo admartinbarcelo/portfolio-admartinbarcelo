93921ec8a7fab70357d09491750c02d4d799be38
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-04-21 18:45:40 +0200

    Actu

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -9,7 +9,7 @@ export const projectsData = [
     image: Work4,
     title: "My Portfolio",
     category: "Frontend",
-    demoLink: "https://admartinbarcelo.netlify.app/",
+    demoLink: "https://github.com/admartinbarcelo/portfolio-admartinbarcelo",
     technologies: [
       <i className="bx bxl-javascript"></i>,
       <i className="bx bxl-react"></i>,
