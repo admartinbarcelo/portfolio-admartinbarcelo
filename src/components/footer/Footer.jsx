efd4e253b8d42254f24aa5f9fcb2f1cf3595b5a1
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-09 18:35:54 +0200

    actualizando

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,65 +1,59 @@
-import './footer.css'
+import "./footer.css";
 
-function Footer () {
-    return(
-        <footer className="footer">
-        <div className="footer__container container">
-          <h1 className="footer__title">Admartinbarcelo</h1>
-  
-          <ul className="footer__list">
-            <li>
-              <a href="#home" className="footer__link">
-                Home
-              </a>
-            </li>
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Admartinbarcelo</h1>
 
-            <li>
-              <a href="#about" className="footer__link">
-                About
-              </a>
-            </li>
-  
-            <li>
-              <a href="#portfolio" className="footer__link">
-                Projects
-              </a>
-            </li>
-  
-          </ul>
-  
-          <div className="footer__social">
-            <a
-              href="https://www.linkedin.com/in/admartinbarcelo/"
-              className="footer__social-link"
-              target="_blank"
-            >
-              <i class='bx bxl-linkedin-square'></i>
+        <ul className="footer__list">
+          <li>
+            <a href="#home" className="footer__link">
+              Home
             </a>
-  
-            <a
-              href="https://github.com/admartinbarcelo"
-              className="footer__social-link"
-              target="_blank"
-            >
-              <i class='bx bxl-github' ></i>
+          </li>
+
+          <li>
+            <a href="#about" className="footer__link">
+              About
             </a>
-  
-            <a
-              href="https://www.instagram.com/admartinbarcelo/"
-              className="footer__social-link"
-              target="_blank"
-            >
-              <i className="bx bxl-instagram"></i>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
             </a>
-          </div>
-  
-          <span className="footer__copy">
-            &#169; Admartinbarcelo. All rigths reserved
-          </span>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com/in/admartinbarcelo/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin-square"></i>
+          </a>
+
+          <a
+            href="https://github.com/admartinbarcelo"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+
+          <a
+            href="https://www.instagram.com/admartinbarcelo/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
         </div>
-      </footer>
-
-    )
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
