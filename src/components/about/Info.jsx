78890cbc8326e6a09d7e873ad6e0b40f7f4a830a
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-28 20:35:11 +0200

    actualizacion

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,7 +7,7 @@ function Info () {
         <i class="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">8 Months, including student period. OPEN TO WORK </span>
+        <span className="about__subtitle">9 Months, including student period. OPEN TO WORK </span>
       </div>
 
       <div className="about__box">
