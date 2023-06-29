eddb852c949f7a664f1fb0335954361c5d2dc0e4
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-06-29 18:52:10 +0200

    adding experience

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -109,6 +109,24 @@ function Qualification() {
                 : "qualification__content"
             }
           >
+          <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Frontend Developer</h3>
+                <span className="qualification__subtitle">
+                  qGenomics - Barcelona
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2023 - Present
+                </div>
+              </div>
+            </div>
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Full-Stack Developer</h3>
