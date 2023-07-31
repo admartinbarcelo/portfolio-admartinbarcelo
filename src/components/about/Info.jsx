263201a33934d74c9fdd42eed80ce770e3208b92
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-07-31 14:54:47 +0200

    fix open to work

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,7 +7,7 @@ function Info () {
         <i class="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">10 Months, including student period. OPEN TO WORK </span>
+        <span className="about__subtitle">10 Months, including student period. </span>
       </div>
 
       <div className="about__box">
