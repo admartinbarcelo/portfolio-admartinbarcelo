fe51e63421367171513aa395cd8a4d8782586970
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-10-31 21:09:08 +0100

    fix months

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,7 +7,7 @@ function Info () {
         <i class="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">10 Months, including student period. </span>
+        <span className="about__subtitle">12 Months, including student period. </span>
       </div>
 
       <div className="about__box">
